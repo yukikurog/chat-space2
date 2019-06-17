511eb7bb94f082f78256e0294fcf25fb7f5ab7ca
yuki.kurog <hndfra@gmail.com>
2019-06-17 21:51:13 +0900

    initial commit

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ gem "font-awesome-rails"
 gem 'devise'
 gem 'carrierwave'
 gem 'mini_magick'
+gem 'rails-i18n', '~> 5.1'
--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja  
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
   end
 end
