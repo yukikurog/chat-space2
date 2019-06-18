c1a73cf6d5c71b5950a3a49f9c637208aef3896a
yuki.kurog <hndfra@gmail.com>
2019-06-18 20:55:25 +0900

    Ajaxを使用して、messages#createが動く

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -11,7 +11,7 @@ class GroupsController < ApplicationController
 
   def create
     @group = Group.new(group_params)
-    binding.pry
+    # binding.pry
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
