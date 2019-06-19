8d2f69306abd90b705255b7dbd84203cc2988e4c
yuki.kurog <hndfra@gmail.com>
2019-06-20 08:46:04 +0900

    非同期で通信されたユーザーを検索して一致するユーザーを取得(controller)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    respond_to do |format|
+      format.html
+      format.json
+    end
   
   def edit
   end
