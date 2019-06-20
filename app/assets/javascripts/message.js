b474d7439ae4768b9ef7159c800b7ff8fd3b8d7c
yuki.kurog <hndfra@gmail.com>
2019-06-20 20:53:42 +0900

    エラー等の修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -13,32 +13,91 @@ $(function() {
   return html;
   }
 
-  function moveToBottom() {
-    $('.chat-main').animate({
-      scrollTop: $('.messages').height()
-    });
+//   function moveToBottom() {
+//     $('.chat-main').animate({
+//       scrollTop: $('.messages').height()
+//     });
 
-  $('.new_message').on('submit', function(e) {
-    e.preventDefault();
-    console.log(this);
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    return false;
-    $.ajax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('#new_message')[0].reset();
-    })
-    .fail(function(){
-      alert('error');
-    })
+//   $('.new_message').on('submit', function(e) {
+//     e.preventDefault();
+//     console.log(this);
+//     var formData = new FormData(this);
+//     var url = $(this).attr('action');
+//     return false;
+//     $.ajax({
+//       url: url,
+//       type: 'POST',
+//       data: formData,
+//       dataType: 'json',
+//       processData: false,
+//       contentType: false
+//     })
+//     .done(function(data){
+//       var html = buildHTML(data);
+//       $('.messages').append(html);
+//       $('#new_message')[0].reset();
+//     })
+//     .fail(function(){
+//       alert('error');
+//     })
+//   });
+// });
+
+function moveToBottom() {
+  $('.messages').animate({
+  scrollTop: $('.messages')[0].scrollHeight
   });
+};
+
+$('#new_message').on('submit', function(e) {
+  e.preventDefault();
+  var formData = new FormData(this);
+  var url = $(this).attr('action');
+  $.ajax({
+    url: url,
+    type: 'POST',
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+  })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('#new_message')[0].reset();
+    $(".submit-btn").prop('disabled', false);
+    moveToBottom();
+  })
+  .fail(function(){
+    alert('error');
+  })
+  function autoUpdate() {
+    setInterval(function() {
+      if($('.message')[0]) {
+        var message_id = $('.message:last').data('id');
+      } else {
+        var message_id = 0;
+      }
+      $.ajax({
+        url: './messages',
+        type: 'GET',
+        data: {id: message_id},
+        dataType: 'json'
+      })
+      .done(function(data) {
+          if(data.length != 0) {
+            var html = buildHTML(data);
+            $('.messages').append(html);
+            moveToBottom();
+          }
+      })
+      .fail(function () {
+        alert('error');
+      })
+    }, 5000);
+  };
+  if (document.location.href.match("/messages")) {
+    autoUpdate();
+  }
+});
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%").limit(10)
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
     respond_to do |format|
       format.html
       format.json
     end
+  end
   
   def edit
   end
